b8ebf0498fa51b5508a9d6af3431490300488077
progToMax <m@m.com>
2019-11-26 22:24:55 -0600

    added nav items and event handlers

--- js/app.js
+++ js/app.js
@@ -1,89 +1,37 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
- */
-
-/**
- * Define Global Variables
- * 
- */
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
- */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
- */
-
-// Build menu 
-var menu = ['Section1', 'Section2', 'Section3'];
-// Scroll to section on link click
-function scrollto() {
-
-}
-// Set sections as active
 (function() {
+    // Build menu 
+
     document.querySelector('.activesection').className = 'active';
+    //array of navbar list items to be used to populate the nav unorder list
     var navbar = [{
-            color: 'green',
             text: 'Section 1',
             target: '#section1'
         },
         {
-            color: 'blue',
             text: 'Section 2',
             target: '#section2'
         },
         {
-            color: 'red',
             text: 'Section 3',
             target: '#section3'
         }
 
     ];
+    /*
+        makeactive event handler used to handle click event to the list item 
+    */
     const makeactive = function(el) {
-        const lists = document.querySelectorAll('#navbar__list li');
-        for (var i = 0; i < document.querySelectorAll('#navbar__list li').length; i++) {
-            const val = lists[i].classList.value;
-            console.log(val);
-            lists[i].classList.remove("activenav");
-        }
-        el.target.classList.add("activenav");
+            const lists = document.querySelectorAll('#navbar__list li');
+            lists.forEach(x => console.log(x));
+            //remove all class activenav to the list items in nav
+            for (var i = 0; i < lists.length; i++) {
+                lists[i].classList.remove("activenav");
+            }
+            //add the activenav class to the clicked on list item 
+            el.target.parentElement.classList.add("activenav");
 
-    }
+        }
+        //populate the navbar with the list items to click on 
     navbar.forEach(x => {
         const navbarlist = document.querySelector('#navbar__list');
         const li = document.createElement('li');
