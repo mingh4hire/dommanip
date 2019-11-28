9bd45f9f438e772b4acc954417f082d77e6ce23c
progToMax <m@m.com>
2019-11-27 18:53:35 -0600

    added review upates

--- js/app.js
+++ js/app.js
@@ -1,44 +1,62 @@
 (function() {
     // Build menu 
 
-    document.querySelector('.activesection').className = 'active';
     //array of navbar list items to be used to populate the nav unorder list
-    var navbar = [{
-            text: 'Section 1',
-            target: '#section1'
-        },
-        {
-            text: 'Section 2',
-            target: '#section2'
-        },
-        {
-            text: 'Section 3',
-            target: '#section3'
-        }
+    // data-nav="Section 1"
 
-    ];
     /*
         makeactive event handler used to handle click event to the list item 
     */
     const makeactive = function(el) {
-            const lists = document.querySelectorAll('#navbar__list li');
-            lists.forEach(x => console.log(x));
-            //remove all class activenav to the list items in nav
-            for (var i = 0; i < lists.length; i++) {
-                lists[i].classList.remove("activenav");
+        const lists = document.querySelectorAll('#navbar__list li');
+        //remove all class activenav to the list items in nav
+        for (let i = 0; i < lists.length; i++) {
+            lists[i].classList.remove("activenav");
+
+            if (lists[i].attributes['data-nav'].value === el) {
+                lists[i].classList.add("activenav");
             }
-            //add the activenav class to the clicked on list item 
-            el.target.parentElement.classList.add("activenav");
 
         }
-        //populate the navbar with the list items to click on 
-    navbar.forEach(x => {
-        const navbarlist = document.querySelector('#navbar__list');
+
+        /*
+            add and remove class "activesection" to the appropriate section elements;
+        */
+        const listsofsecs = document.querySelectorAll("section");
+        listsofsecs.forEach(x => {
+            x.classList.remove('activesection');
+
+            if (x.attributes['data-nav'].value === el) {
+                x.classList.add('activesection');
+                const bodyRect = document.body.getBoundingClientRect();
+                elemRect = x.getBoundingClientRect();
+                const offset = elemRect.top - bodyRect.top;
+
+                scrollTo(0, offset)
+            }
+        });
+        //add the activenav class to the clicked on list item 
+
+    }
+
+
+    //populate the navbar with the list items to click on 
+
+    var navbarsecs = document.querySelectorAll("section");
+    const navbarlist = document.querySelector('#navbar__list');
+
+    for (let i = 0; i < navbarsecs.length; i++) {
+        var datanav = navbarsecs[i].attributes['data-nav'].value;
         const li = document.createElement('li');
-        li.innerHTML = `<a href="${x.target}">${x.text}</a>`;
-        li.onclick = makeactive;
+        li.setAttribute('data-nav', datanav);
+        li.innerHTML = datanav;
+        const nav = datanav;
+
+        li.addEventListener('click', function() {
+            makeactive(nav);
+        });
         navbarlist.appendChild(li);
-    });
+    }
 
 
 })();
